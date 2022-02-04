8a7e570d4d3f38886a3a4d2c495b85b98b866321
luckson1 <jacksongathondu@gmail.com>
2022-02-04 03:15:56 +0300

    Final Commit
    The App is now complete and working

--- crypto-prices/src/App.js
+++ crypto-prices/src/App.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Coin } from "./Coin";
+import "./App.css"
 // https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
 function App() {
